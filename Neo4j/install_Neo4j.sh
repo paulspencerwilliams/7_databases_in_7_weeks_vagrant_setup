6a70f2c788ec6b8a9f6b2551df1dd846012fc82d
Paul Williams <paulswilliams@me.com>
2013-04-21 19:10:55 +0100

    renamed install script

--- Neo4j/install_Neo4j.sh
+++ Neo4j/install_Neo4j.sh
@@ -4,4 +4,4 @@ echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.
 apt-get update && apt-get -y install neo4j-enterprise
 sed "s/#org.neo4j.server.webserver.address/org.neo4j.server.webserver.address/g" /etc/neo4j/neo4j-server.properties > /etc/neo4j/neo4j-server-updated.properties
 cat /etc/neo4j/neo4j-server-updated.properties > /etc/neo4j/neo4j-server.properties
-#sudo sh -c 'cat /etc/neo4j/neo4j-server-updated.properties > /etc/neo4j/neo4j-server.properties'
+/etc/init.d/neo4j-service restart
