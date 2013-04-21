6c638f2fc83a210f374980203444c494bdfd3d68
Paul Williams <paulswilliams@me.com>
2013-04-21 18:40:33 +0100

    Initial structure with Neo4j setup

---
+++ Neo4j/install_neoj.sh
@@ -0,0 +1,7 @@
+wget -O - http://debian.neo4j.org/neotechnology.gpg.key|  apt-key add -
+touch /etc/apt/sources.list.d/neo4j.list
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+apt-get update && apt-get -y install neo4j-enterprise
+sed "s/#org.neo4j.server.webserver.address/org.neo4j.server.webserver.address/g" /etc/neo4j/neo4j-server.properties > /etc/neo4j/neo4j-server-updated.properties
+cat /etc/neo4j/neo4j-server-updated.properties > /etc/neo4j/neo4j-server.properties
+#sudo sh -c 'cat /etc/neo4j/neo4j-server-updated.properties > /etc/neo4j/neo4j-server.properties'
