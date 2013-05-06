c7d952525254389449a83a6c577bc7ff4aa7b5ed
Paul Williams <paulswilliams@me.com>
2013-05-06 18:51:12 +0100

    added Neo4j support

--- Polygot/provision.sh
+++ Polygot/provision.sh
@@ -37,3 +37,12 @@ apt-get -y install nodejs
 
 npm install hiredis redis csv
 apt-get install couchdb -y
+
+
+wget -O - http://debian.neo4j.org/neotechnology.gpg.key|  apt-key add -
+touch /etc/apt/sources.list.d/neo4j.list
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+apt-get update && apt-get -y install neo4j-enterprise
+sed "s/#org.neo4j.server.webserver.address/org.neo4j.server.webserver.address/g" /etc/neo4j/neo4j-server.properties > /etc/neo4j/neo4j-server-updated.properties
+cat /etc/neo4j/neo4j-server-updated.properties > /etc/neo4j/neo4j-server.properties
+/etc/init.d/neo4j-service restart
